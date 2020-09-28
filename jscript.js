a96ac7ba2d5a0a89d94efb892583b3e1850daad7
KLIJIN <mdu091008@yandex.ru>
2020-09-28 22:17:14 +0700

    калькулятор 1.0

---
+++ jscript.js
@@ -0,0 +1,19 @@
+function insert(num) {
+    document.form.textviev.value += num;
+}
+
+
+function clean() {
+    document.form.textviev.value = "";
+}
+
+function back() {
+    let exp = document.form.textviev.value;
+    document.form.textviev.value = exp.substring(0, exp.length -1);
+};
+function equal () {
+    let exp = document.form.textviev.value;
+    if (exp) {
+        document.form.textviev.value = eval(exp); //eval принимает в себя строку и высчитывает результат
+    }
+}
